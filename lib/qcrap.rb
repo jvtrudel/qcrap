6d472e95bba83f4f6b456e0b646155a05cc080dc
trud <jeremy.vtrudel@cidco.ca>
2017-09-27 00:12:47 -0400

    plan de match pour le parser jaune

--- lib/qcrap.rb
+++ lib/qcrap.rb
@@ -1,5 +1,33 @@
 require "qcrap/version"
+require "nokogiri"
+require "open-uri"
+require "rubygems"
 
 module Qcrap
-  # Your code goes here...
+  # Parser
+  Class yellowParser {
+    @parsed=[]
+    @page=Nokogiri::HTML::Document
+    # Récupère une page html
+    def fetch(url) {
+       puts "fetch"
+    }
+    # Détermine le nombre d'entrés
+    def numberOfEntries {
+      puts "numberOfEntries"
+    }
+    def nextPage {
+       puts "nextPage"
+    }
+    # Récupère les données pertinentes d'une en
+    Function parseEntry(n) {
+      #page.css('div#Contact11').css('h2#ContactName11').css('a')[0].children.text
+      puts "parseEntry"
+    }
+    # Récupère toutes les entrés
+    Function parse {
+         puts "parse"
+    }
+
+  }
 end
